bcb9de61e67339d848c7c6e70f7e8797f200f87e
Student <kendyivy@gmail.com>
2018-07-18 16:35:47 +0300

    added content

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="container">
+  <div class="topnav">
+          <a class="active" href="#search">search</a>
+            
+            <input  v-model="search"type="text" placeholder="Search..">
+            
+            <button v-on:click="loadNews"type="button">Search</button>
+            <p v-if="loading">Please wait...</p>
+            
+        </div>
+        <div class="loader" v-if="loading"></div>
    <div v-for="article in news">
-      <img :src="article.urlToImage" alt=""> 
+
+      <img  v-if="article.urlToImage" :src="article.urlToImage" alt=""> 
      <h3>{{article.title}}</h3>
-     <h4>{{article.author}}</h4>
+     <div> class="column"
+     <h4>{{article.author}}</h4></div>
+
+     <blockquote>
      <p>{{article.description}}</p>
+     </blockquote>
+    <a href :"article.url"target="blank" class="button">Read More</a>
+
+
 
     </div>
 
@@ -13,10 +31,16 @@
 
 <script>
 import axios from 'axios'
-let url= "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=98b4d0418d994067b20bc487a55d994e"
+
+const baseUrl = "https://newsapi.org/v2/";
+const apiKey="98b4d0418d994067b20bc487a55d994e";
+const endpoint= "/everything";
+
 
 const data={
-news:[]
+news:[],
+search: '',
+loading:false
 }
 export default {
   data: function() {
@@ -28,8 +52,20 @@ export default {
   },
   methods: {
   	loadNews(){
+     if(this.search.length<1){
+      return;
+     }
+     
+     this.loading = true;
+     this.news = [];
+
+
+     let url=baseUrl + endpoint + '?q=' + this.search + '&apiKey=' + apiKey;
+
+
     axios.get(url).then(function(response){
     console.log(response.data.articles)
+    data.loading = false;
     data.news=response.data.articles
     }).catch(function(error) {
     console.log(error.message)
